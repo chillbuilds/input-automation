1641f00edf3facbd5b0a10fc63957215ce1c1615
chillbuilds <will@chillbuilds.com>
2020-04-24 21:18:48 -0400

    automating google image search for sandwich

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ var screenSize = robot.getScreenSize()
 
 robot.setMouseDelay(2)
 
-robot.moveMouse(screenSize.width/2, screenSize.height/2)
+robot.moveMouse(screenSize.width/100*3.5, screenSize.height/100*98.5)
+robot.mouseClick('left', false)
+setTimeout(function(){
+    robot.typeString('images.google.com')
+    robot.keyTap('enter')
+    setTimeout(() => {
+    robot.typeString('sandwich')
+    robot.keyTap('enter')
+}, 2000);
+}, 3000);
